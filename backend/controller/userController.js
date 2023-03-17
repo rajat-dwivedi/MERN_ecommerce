232b430039b8358c0b50892f317e17896247d5ac
rajat-dwivedi <rdroid302@gmail.com>
2023-03-17 16:46:56 -0500

    feature to update the password

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -128,3 +128,32 @@ exports.resetPassword = catchAsyncError( async (req,res,next)=>{
 
     sendToken(user,200,res);
 })
+
+//getting user details
+exports.getUserDetails = catchAsyncError(async (req,res,next)=>{
+    const user = await User.findById(req.user.id)
+    res.status(200).json({
+        success : true,
+        user
+    })
+})
+
+exports.updatePassword = catchAsyncError(async (req,res,next)=>{
+    const user = await User.findById(req.user.id).select("+password")
+
+    const isPasswordMatched = await user.comparePassword(req.body.oldPassword);
+
+    if(!isPasswordMatched){
+        return next(new ErrorHandler("Oldpassword is incorrect",400));
+    }
+
+    if(req.body.newPassword !== req.body.confirmPassword){
+        return next(new ErrorHandler("password mismatch",400));
+    }
+
+    user.password = req.body.newPassword
+
+    await user.save()
+
+    sendToken(user, 200, res);
+})
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -4,9 +4,12 @@ const {
   logout,
   forgotPassword,
   resetPassword,
+  getUserDetails,
+  updatePassword,
 } = require("../controller/userController");
 
 const express = require("express");
+const { isAuthenticatedUser } = require("../middleware/auth");
 
 const router = express.Router();
 
@@ -15,5 +18,7 @@ router.route("/login").post(loginUser);
 router.route("/password/forgot").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
 router.route("/logout").get(logout);
+router.route("/me").get(isAuthenticatedUser, getUserDetails)
+router.route("/password/update").put(isAuthenticatedUser, updatePassword)
 
 module.exports = router;
